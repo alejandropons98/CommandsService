6b611bb8e7da1ec8f911e9bf9442195b6b192c63
alejandropons98 <alejandro.pons@correo.unimet.edu.ve>
2023-03-08 11:28:36 -0400

    Add project files.

---
+++ CommandsService/Models/ApiResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace CommandsService.Models
+{
+    public class ApiResponse<T>
+    {
+        public bool Success { get; set; }
+        public String? CodeResult { get; set; }
+        public String? Message { get; set; }
+        public T? Data { get; set; }
+    }
+}
